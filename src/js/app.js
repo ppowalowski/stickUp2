7f9a085f19093ad23c900b23b110cb0d41f53ed5
ppowalowski <patrik@powalowski.de>
2015-05-12 19:33:19 +0200

    prettify gh-page

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ var app = (function (document, $) {
                     zIndex:100
                 });
                 $('.sticky-keep').stickUp({
-                    topMargin:130,
+                    topMargin:90,
                     keepInWrapper:true,
                     wrapperSelector:'.row.container'
                 });
@@ -21,12 +21,12 @@ var app = (function (document, $) {
                 });
                 $('.sidebar').stickUp({
                     keepInWrapper:true,
-                    topMargin:130,
+                    topMargin:90,
                     wrapperSelector:'.row.container'
                 });
                 $('.sidebar-1').stickUp({
                     keepInWrapper:true,
-                    topMargin:130,
+                    topMargin:90,
                     wrapperSelector:'.row.container'
                 });
             };
