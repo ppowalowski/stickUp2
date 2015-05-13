60bff45048ba169c1b71b808b7db47557eb0d192
ppowalowski <patrik@powalowski.de>
2015-05-13 20:18:43 +0200

    strip logs on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require('gulp'), //
-    concat = require('gulp-concat'), //
-    uglify = require('gulp-uglify');
+var gulp = require('gulp'), 
+    concat = require('gulp-concat'), 
+    uglify = require('gulp-uglify'),
+    stripDebug = require('gulp-strip-debug');
 
 gulp.task('js', function () {
     return gulp.src('src/stickUp.js')
+        .pipe(stripDebug())
+        .pipe(gulp.dest('build/js/'))
         .pipe(uglify())
         .pipe(concat('stickUp.min.js'))
         .pipe(gulp.dest('build/js/'));
